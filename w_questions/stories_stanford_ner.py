d2192056466025d0efc668008162cc74ef9a2e47
Arthur Haas <arthur.haas4@gmail.com>
2021-05-14 23:30:34 +0200

    kinda pull request review :)

--- w_questions/stories_stanford_ner.py
+++ w_questions/stories_stanford_ner.py
@@ -1,60 +1,72 @@
-#%%
+# %%
 import os
 import nltk
 import json
 
-#config
+# https://stackoverflow.com/questions/22081209/find-the-root-of-the-git-repository-where-the-file-lives#comment44778829_22081487
+import sys, subprocess
+repo_dir = subprocess.Popen(['git', 'rev-parse', '--show-toplevel'], stdout=subprocess.PIPE).communicate()[0].rstrip().decode('utf-8')
+sys.path.insert(1, os.path.join(sys.path[0], repo_dir))
+from helper.HolmesReader import HolmesReader
+
+# config
 PATH_DATASET = "dataset"
 PATH_INPUT = "processed"
 PATH_STORIES = "stories"
 PATH_VALIDATION = "validation_set"
 FILENAME_NAMED_ENTITIES = "story_named_entities.json"
-NAMED_ENTITIES = {}
-PATH_STANFORD_NER = os.path.join('..',"libraries","stanford-ner-4.2.0","stanford-ner-2020-11-17")
+PATH_STANFORD_NER = os.path.join(
+    '..', "libraries", "stanford-ner-4.2.0", "stanford-ner-2020-11-17")
 
-#define the Stanford NER tagger
-classifier = os.path.join(PATH_STANFORD_NER,"classifiers","english.muc.7class.distsim.crf.ser.gz")
-jar = os.path.join(PATH_STANFORD_NER,"stanford-ner.jar")
+# define the Stanford NER tagger
+classifier = os.path.join(PATH_STANFORD_NER, "classifiers",
+                          "english.muc.7class.distsim.crf.ser.gz")
+jar = os.path.join(PATH_STANFORD_NER, "stanford-ner.jar")
 st = nltk.tag.StanfordNERTagger(classifier, jar, encoding='utf-8')
 
-#%%
+# helper for reading stories & novels
+holmesReader = HolmesReader()
+
+# %%
+
+
 def named_entities():
     """
     Finds named entities in stories. 
     """
-    FOLDER_PATH = os.path.join('..',PATH_DATASET,PATH_INPUT,PATH_STORIES)
-    for collection in os.listdir(FOLDER_PATH):
-        PATH_COLLECTION = os.path.join(FOLDER_PATH,collection)
+    NAMED_ENTITIES = {}
 
-        for f in os.listdir(PATH_COLLECTION):
-            filepath = os.path.join(PATH_COLLECTION,f)
+    collections = holmesReader.get_collections()
 
-            if os.path.isfile(filepath) and f.endswith(".txt"):
-                print(f"Process file: {filepath}")
+    for collection_id, collection in collections.items():
+        if len(NAMED_ENTITIES.keys()) > 0:
+            continue
+        for story_id, story in collection['stories'].items():
+            if len(NAMED_ENTITIES.keys()) > 0:
+                continue
+            print(f"Collection {collection_id}: story {story_id}")
+            content = story['text']
 
-                with open(filepath, "r") as fp:
-                    content = fp.read()
-                    #tokenize text by words
-                    tokenized_content = nltk.tokenize.word_tokenize(content)
-                    #tag content by Stanford NER tool
-                    tagged_content = st.tag(tokenized_content)
-                    #POS tagging
-                    pos_tags = nltk.pos_tag(tokenized_content)
-                    #join info about POS tag and named entity tag
-                    triples = [[name, pos, tp[1]] for (name,pos), tp in zip(pos_tags, tagged_content)]
-                    #create tree from the tuples
-                    tree = IOB_to_tree(triples)
-                    #extract named entities (persons, locations, times)
-                    [person, location, time] = get_entities(tree)
-                    #save named entites to dictionary
-                    filename = f.replace('.txt','')
-                    NAMED_ENTITIES[filename] = {}
-                    NAMED_ENTITIES[filename]["characters"] = person
-                    NAMED_ENTITIES[filename]["locations"] = location
-                    NAMED_ENTITIES[filename]["dates and times"] = time
+            # tokenize text by words
+            tokenized_content = nltk.tokenize.word_tokenize(content)
+            # tag content by Stanford NER tool
+            tagged_content = st.tag(tokenized_content)
+            # POS tagging
+            pos_tags = nltk.pos_tag(tokenized_content)
+            # join info about POS tag and named entity tag
+            triples = [[name, pos, tp[1]] for (name, pos), tp in zip(pos_tags, tagged_content)]
+            # create tree from the tuples
+            tree = IOB_to_tree(triples)
+            # extract named entities (persons, locations, times)
+            [person, location, time] = get_entities(tree)
+            # save named entites to dictionary
+            NAMED_ENTITIES[story_id] = {}
+            NAMED_ENTITIES[story_id]["characters"] = person
+            NAMED_ENTITIES[story_id]["locations"] = location
+            NAMED_ENTITIES[story_id]["dates and times"] = time
+    return NAMED_ENTITIES
 
 
-#%%
 def get_entities(tree):
     """
     Finds named entities of the tree created from the tagged content.
@@ -77,7 +89,6 @@ def get_entities(tree):
     return person, location, time
 
 
-#%%
 def IOB_to_tree(iob_tagged):
     """
     From the given tuples (entity name, POS tag, entity type) creates a tree.
@@ -97,7 +108,6 @@ def IOB_to_tree(iob_tagged):
     return root
 
 
-#%%
 def save_entities(ENTITIES):
     """
     Saves named entities into the json file.
@@ -106,9 +116,11 @@ def save_entities(ENTITIES):
         parsed = json.dumps(ENTITIES, indent=4, sort_keys=True)
         fp.write(parsed)
 
-#%%
+
+# %%
 if __name__ == "__main__":
     print("start")
-    named_entities()
-    save_entities(NAMED_ENTITIES)
+    entities = named_entities()
+    save_entities(entities)
+    print("done")
 # %%
