4bca5658a2d783944c6e1bd4ffdfe0063bb199ae
Arthur Haas <arthur.haas4@gmail.com>
2021-05-28 00:01:28 +0200

    add final

--- helper/format_metadata.py
+++ helper/format_metadata.py
@@ -28,3 +28,40 @@ if __name__ == "__main__":
         pretty_print = json.dumps(metadata, indent=4, sort_keys=True)
         f.write(pretty_print)
 
+# %%
+
+import json
+import os
+import pandas as pd
+
+
+if __name__ == "__main__":
+
+    with open("metadata.json", "r") as f:
+        metadata = json.load(f)
+
+    flatten = []
+    cols = "collection,title,publishdate,setdate".split(",")
+
+    for n, novel in metadata['novels'].items():
+        flatten.append([
+            "novel",
+            n,
+            novel['publish_date'],
+            novel['plot']['start_date']
+        ])
+
+
+    for c, collection in metadata['collections'].items():
+        for s, story in collection['stories'].items():
+            flatten.append([
+                c,
+                s,
+                story['publish_date'],
+                story['plot']['start_date']
+            ])
+
+
+    df_flatten = pd.DataFrame(flatten, columns=cols)
+    df_flatten.to_csv("metadata_flatten.csv", index=False)
+# %%
