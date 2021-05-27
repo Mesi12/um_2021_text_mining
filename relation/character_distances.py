efc64bb333bc20f7a0d9c60739112277cf35409a
Arthur Haas <arthur.haas4@gmail.com>
2021-05-27 19:07:27 +0200

    do all stories and add metric function

--- relation/character_distances.py
+++ relation/character_distances.py
@@ -23,6 +23,10 @@ def plot_half_gaussian():
     plt.show()
 
 
+def smooth_metric(distance):
+    return distance
+
+
 def generate_nodes_edges(df):
 
     #extract characters
@@ -32,54 +36,63 @@ def generate_nodes_edges(df):
 
     #group by stories
     df_grouped = df[df["ner"]=="PERSON"].groupby(by="title",sort="False")
-    data = []
-    story_name= "the_hound_of_the_baskervilles"
-    df_story = df_grouped.get_group(story_name)
-    position = list(df_story["docTokenBegin"])
-    max_token = max(position)
-    characters = list(df_story[COL_FOR_GRAPH])
-    for i in range(len(position)):
-        for j in range(i+1,len(position)):
-            if (characters[i] != characters[j]):
-                dist = abs(position[i]-position[j])
-                data.append([characters[i],characters[j],dist,story_name,max_token])
-    df_distances2 = pd.DataFrame(data, columns = ["character1", "character2","distance","story","max_token"])
-
-    #merge first two columns of df_distances2
-    a = df_distances2["character1"]
-    b = df_distances2["character2"]
-    merged = []
-    for i in range(len(a)):
-        merged.append((" | ".join(sorted([a[i],b[i]]))))
-    merged
-    df_distances2["merged"] = merged
-
-
-    df_dist_group = df_distances2.groupby(["story","merged"]).agg({"distance": "sum"})
-    df_dist_group = df_dist_group.reset_index()
-
-    df_dist_group["distanceNorm"] = df_dist_group['distance'] / max_token
-
+    stories = list(df['title'].unique())
+    edges_frames = []
     
-    df_edges = df_dist_group.groupby(by="merged").agg({"distanceNorm": np.mean})
-    df_edges = df_edges.reset_index()
-    df_edges[['source_name', 'target_name']] = pd.DataFrame(
-        df_edges['merged'].str.split('|').tolist(),
-        columns = ['source_name','target_name'])
-    df_edges['source_name'] = df_edges['source_name'].str.strip()
-    df_edges['target_name'] = df_edges['target_name'].str.strip()
+    for story_i, story_name in enumerate(stories):
+        print(f"{story_i+1}/{len(stories)} - {story_name}")
+        data = []
+        df_story = df_grouped.get_group(story_name)
+        positions = list(df_story["docTokenBegin"])
+        max_token = max(positions)
+        characters = list(df_story[COL_FOR_GRAPH])
+        for i in range(len(positions)):
+            for j in range(i+1,len(positions)):
+                if (characters[i] != characters[j]):
+                    dist = abs(positions[i]-positions[j])
+                    data.append([characters[i],characters[j],dist,story_name,max_token])
+        df_distances2 = pd.DataFrame(data, columns = ["character1", "character2","distance","story","max_token"])
 
-    df_edges = df_edges.merge(
-        df_person[['index', COL_FOR_GRAPH]],
-        left_on="source_name",
-        right_on=COL_FOR_GRAPH)
-    df_edges = df_edges.rename(columns={"index": "source"}).drop(COL_FOR_GRAPH, axis=1)
+        #merge first two columns of df_distances2
+        a = df_distances2["character1"]
+        b = df_distances2["character2"]
+        merged = []
+        for i in range(len(a)):
+            merged.append((" | ".join(sorted([a[i],b[i]]))))
+        merged
+        df_distances2["merged"] = merged
 
-    df_edges = df_edges.merge(
-        df_person[['index', COL_FOR_GRAPH]],
-        left_on="target_name",
-        right_on=COL_FOR_GRAPH)
-    df_edges = df_edges.rename(columns={"index": "target"}).drop(COL_FOR_GRAPH, axis=1)
+
+        df_dist_group = df_distances2.groupby(["story","merged"]).agg({"distance": "sum"})
+        df_dist_group = df_dist_group.reset_index()
+
+        df_dist_group["distanceNorm"] = df_dist_group['distance'].apply(lambda x: smooth_metric(x))
+
+        
+        df_edges = df_dist_group.groupby(by="merged").agg({"distanceNorm": np.mean})
+        df_edges = df_edges.reset_index()
+        df_edges[['source_name', 'target_name']] = pd.DataFrame(
+            df_edges['merged'].str.split('|').tolist(),
+            columns = ['source_name','target_name'])
+        df_edges['source_name'] = df_edges['source_name'].str.strip()
+        df_edges['target_name'] = df_edges['target_name'].str.strip()
+
+        df_edges = df_edges.merge(
+            df_person[['index', COL_FOR_GRAPH]],
+            left_on="source_name",
+            right_on=COL_FOR_GRAPH)
+        df_edges = df_edges.rename(columns={"index": "source"}).drop(COL_FOR_GRAPH, axis=1)
+
+        df_edges = df_edges.merge(
+            df_person[['index', COL_FOR_GRAPH]],
+            left_on="target_name",
+            right_on=COL_FOR_GRAPH)
+        df_edges = df_edges.rename(columns={"index": "target"}).drop(COL_FOR_GRAPH, axis=1)
+
+        edges_frames.append(df_edges)
+
+
+    df_edges = pd.concat(edges_frames)
 
     return (df_person, df_edges)
 
@@ -96,6 +109,7 @@ if __name__ == "__main__":
 
     file = os.path.join('..', 'w_questions', 'output_parsed', 'ner', '_merged_.csv')
     df = pd.read_csv(file)
+    df = df.drop("nerConfidences", axis=1)
 
     df_person, df_edges = generate_nodes_edges(df)
 
